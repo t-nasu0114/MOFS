4733baa4411364c36c00b39a3e8111f54b75d144
t-nasu0114 <sagara_misako@outlook.jp>
2026-03-05 00:54:37 +0900

    fix: Fix directory structure
    Fix directory structure as follows
    
    ```
    mofs_project/
    ├── core/
    │   └── mofs_format.c     <-- common
    │
    ├── os/
    │   └── linux             <-- OS dependent
    │       └── port_linux.c
    │
    └── proc/
        ├── linux/
        │   ├── mkfs/
        │   │   └── main.c    <-- linux mkfs
        │   └── fuse/
        │       └── main.c     <-- linux fuse
        │
        └── zephyr/
            └── demo/
                └── main.c     <-- zephyr
    ```

--- src/core/mofs_format.c
+++ src/core/mofs_format.c
@@ -6,15 +6,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-
-static void print_usage(const char *prog_name)
-{
-    MOFS_INF("Usage: %s [OPTIONS] DEVICE_FILE\n", prog_name);
-    MOFS_INF("Options:\n");
-    MOFS_INF("  -s, --size <NUM>   Specify file system size in blocks (default: auto)\n");
-    MOFS_INF("  -h, --help         Display this help message\n");
-}
 
 static int clear_blocks(int fd, uint64_t block_num)
 {
@@ -38,7 +29,7 @@ static int clear_blocks(int fd, uint64_t block_num)
     return ret;
 }
 
-static int mofs_format(const char *device_file, int fs_size, int blk_size)
+int mofs_format(const char *device_file, int fs_size, int blk_size)
 {
     int ret = 0;
     int fd;
@@ -161,55 +152,4 @@ out2:
     dev_close(fd);
 out1:
     return ret;
-}
-
-int main(int argc, char *argv[])
-{
-    int   fd;
-    int   opt;
-    int   fs_size  = -1;
-    int   blk_size = -1;
-    char *device_file;
-
-    while ((opt = getopt(argc, argv, "s:b:h")) != -1) {
-        switch (opt) {
-        case 's':
-            fs_size = atoi(optarg);
-            break;
-        case 'b':
-            blk_size = atoi(optarg);
-            if (blk_size != MOFS_BLK_SIZE) {
-                MOFS_ERR("Only %d block size is supported.\n", MOFS_BLK_SIZE);
-                return 1;
-            }
-            break;
-        case 'h':
-            print_usage(argv[0]);
-            return 0;
-        default:
-            /* nothing */
-            break;
-        }
-    }
-
-    /* Check device file name */
-    if (optind < argc) {
-        device_file = argv[optind];
-    } else {
-        MOFS_ERR("Device file is required.\n");
-        print_usage(argv[0]);
-        return 1;
-    }
-
-    // MOFS_DBG("Format Configuration:\n");
-    // MOFS_DBG("  Target Device: %s\n", device_file);
-    // MOFS_DBG("  FS Size:       %d blocks\n", fs_size);
-
-    /* Format device */
-    if (mofs_format(device_file, fs_size, blk_size) != 0) {
-        MOFS_ERR("Format error\n");
-        return 1;
-    }
-
-    return 0;
 }
---
+++ src/proc/linux/mkfs_mofs/mkfs_mofs.c
@@ -0,0 +1,67 @@
+
+#include <mofs_devio.h>
+#include <mofs_errno.h>
+#include <mofs_log.h>
+#include <mofs_struct.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* TODO: This extern will be deleted */
+extern int mofs_format(const char *device_file, int fs_size, int blk_size);
+
+static void print_usage(const char *prog_name)
+{
+    MOFS_INF("Usage: %s [OPTIONS] DEVICE_FILE\n", prog_name);
+    MOFS_INF("Options:\n");
+    MOFS_INF("  -s, --size <NUM>   Specify file system size in blocks (default: auto)\n");
+    MOFS_INF("  -h, --help         Display this help message\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int   fd;
+    int   opt;
+    int   fs_size  = -1;
+    int   blk_size = -1;
+    char *device_file;
+
+    while ((opt = getopt(argc, argv, "s:b:h")) != -1) {
+        switch (opt) {
+        case 's':
+            fs_size = atoi(optarg);
+            break;
+        case 'b':
+            blk_size = atoi(optarg);
+            if (blk_size != MOFS_BLK_SIZE) {
+                MOFS_ERR("Only %d block size is supported.\n", MOFS_BLK_SIZE);
+                return 1;
+            }
+            break;
+        case 'h':
+            print_usage(argv[0]);
+            return 0;
+        default:
+            /* nothing */
+            break;
+        }
+    }
+
+    /* Check device file name */
+    if (optind < argc) {
+        device_file = argv[optind];
+    } else {
+        MOFS_ERR("Device file is required.\n");
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    /* Format device */
+    if (mofs_format(device_file, fs_size, blk_size) != 0) {
+        MOFS_ERR("Format error\n");
+        return 1;
+    }
+
+    return 0;
+}
