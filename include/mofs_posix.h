ae0c4e635deac743c5be8849fec9509b0d09f814
t-nasu0114 <sagara_misako@outlook.jp>
2026-05-30 16:15:05 +0900

    Change mofs POSIX header name

--- include/mofs_posix.h
+++ include/mofs_posix.h
@@ -1,10 +1,10 @@
 #ifndef __MOFS_POSIX__
 #define __MOFS_POSIX__
 
-#include <posix/dirent.h>
-#include <posix/fcntl.h>
-#include <posix/limits.h>
-#include <posix/sys/stat.h>
-#include <posix/unistd.h>
+#include <posix/mofs_dirent.h>
+#include <posix/mofs_fcntl.h>
+#include <posix/mofs_limits.h>
+#include <posix/sys/mofs_stat.h>
+#include <posix/mofs_unistd.h>
 
 #endif /* __MOFS_POSIX__ */
